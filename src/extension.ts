3bf70508a369af807043671deb7dfa86ede550c8
Carlos Pita <carlosjosepita@gmail.com>
2024-03-31 04:48:16 -0300

    Simple pdf preview command

--- src/extension.ts
+++ src/extension.ts
@@ -24,22 +24,15 @@ async function previewPdf(extensionUri: vscode.Uri) {
 			]
 		}
 	);
-	const resolveUri = (path: string) => panel.webview.asWebviewUri(
-		vscode.Uri.joinPath(extensionUri, 'pdfjs', path)
-	);
-	//const pdfUrl = resolveUri('web/compressed.tracemonkey-pldi-09.pdf');
-	const pdfUrl = resolveUri('web/r.pdf');
-	const customHtml = `
-<link rel="resource" type="application/l10n" href="${resolveUri('web/locale/locale.json')}">
-<link rel="stylesheet" href="${resolveUri('web/viewer.css')}">
-<script src="${resolveUri('build/pdf.mjs')}" type="module"></script>
-<script src="${resolveUri('web/viewer.mjs')}" type="module"></script>
-<script> var pdfUrl = "${pdfUrl}"; </script>
-`;
+	const pdfjsUri = vscode.Uri.joinPath(extensionUri, 'pdfjs');
 	const htmlData = await vscode.workspace.fs.readFile(
-		vscode.Uri.joinPath(extensionUri, 'assets/pdf-viewer.html')
+		vscode.Uri.joinPath(pdfjsUri, 'web', 'viewer.html')
 	);
-	const html = Buffer.from(htmlData).toString('utf8');
-	panel.webview.html = html.replace('@@@custom@@@', customHtml);
-	console.log(resolveUri('web/compressed.tracemonkey-pldi-09.pdf').toString());
+	let html = Buffer.from(htmlData).toString('utf8');
+	const baseUri = panel.webview.asWebviewUri(pdfjsUri);
+	html = html.replaceAll('{{baseUri}}', baseUri.toString());
+	const docUri = vscode.Uri.joinPath(baseUri, 'web', 'compressed.tracemonkey-pldi-09.pdf');
+	html = html.replaceAll('{{docUri}}', docUri.toString());
+	panel.webview.html = html;
+	console.log(html);
 }
