9f75281b0c87c4e614dfcad82dfb29fd61dc1b85
Carlos Pita <carlosjosepita@gmail.com>
2024-04-06 18:14:55 -0300

    Small hover refactor

--- src/hover.ts
+++ src/hover.ts
@@ -7,7 +7,7 @@ let MathJax: any;
 
 export function* registerProviders() {
   const config = vscode.workspace.getConfiguration('sidepeek.hover');
-  for (let language of config.languages) {
+  for (const language of config.languages) {
     yield vscode.languages.registerHoverProvider(
       { language }, new MathHoverProvider()
     );
@@ -23,10 +23,11 @@ class MathHoverProvider implements vscode.HoverProvider {
       for (let n = position.line; n !== toN; n += dir) {
         const j = n === position.line ? position.character : undefined;
         const text = document.lineAt(n).text;
-        for (let delimiter of delimiters) {
+        for (const delimiter of delimiters) {
+          const { left, right } = delimiter;
           const [i, offset] = dir === -1 ?
-            [text.lastIndexOf(delimiter.left, j), delimiter.left.length] :
-            [text.indexOf(delimiter.right, j), 0];
+            [text.lastIndexOf(left, j && j - 1), left.length] :
+            [text.indexOf(right, j), 0];
           if (i === -1) continue;
           const position = new vscode.Position(n, i + offset);
           return { position, delimiter };
@@ -43,13 +44,12 @@ class MathHoverProvider implements vscode.HoverProvider {
     if (!rightMatch) return;
     const range = new vscode.Range(leftMatch.position, rightMatch.position);
     const tex = document.getText(range).trim();
-    const svg = await tex2svg(tex, leftMatch.delimiter.inline);
-    const contents = new vscode.MarkdownString(`![](${svg})`);
-    return new vscode.Hover(contents, range);
+    const md = await tex2md(tex, leftMatch.delimiter.inline);
+    return new vscode.Hover(new vscode.MarkdownString(md), range);
   }
 }
 
-async function tex2svg(tex: string, inline: boolean) {
+async function tex2md(tex: string, inline: boolean) {
   if (!MathJax) {
     const options = { loader: { load: ['input/tex', 'output/svg'] } };
     MathJax = await require('mathjax').init(options);
@@ -57,10 +57,10 @@ async function tex2svg(tex: string, inline: boolean) {
   const dark = [
     vscode.ColorThemeKind.Dark, vscode.ColorThemeKind.HighContrast
   ].includes(vscode.window.activeColorTheme.kind);
-  let css = `* { font-size: 115%; color: ${dark ? 'white' : 'black'}; }`;
+  let css = `* { font-size: 110%; color: ${dark ? 'white' : 'black'}; }`;
   let svg = MathJax.tex2svg(tex, { display: !inline });
   svg = MathJax.startup.adaptor.innerHTML(svg);
   svg = svg.replace(/<defs>/, `<defs><style>${css}</style>`);
   const base64 = Buffer.from(svg).toString('base64');
-  return 'data:image/svg+xml;base64,' + base64;
+  return `![](data:image/svg+xml;base64,${base64})`;
 }
